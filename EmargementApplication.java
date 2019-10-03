24b865bbe9ca2ba56d7ff2b1872a4c86a579ebe3
Nicolas MYLLE <millt.nico@hotmail.fr>
2019-10-03 13:59:41 +0200

    Init

---
+++ EmargementApplication.java
@@ -0,0 +1,13 @@
+package com.lacatho.miii.emargement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmargementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmargementApplication.class, args);
+    }
+
+}
