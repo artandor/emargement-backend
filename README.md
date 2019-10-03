17f9ce9a033f6509cb508e7c2f78c5eeb9a01eb9
Nicolas MYLLE <millt.nico@hotmail.fr>
2019-10-03 14:03:58 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,8 @@
+#Emargement - Backend
+
+Projet de groupe 2019 - 2020 M2 III
+Membres : 
+- Hanen BENMANSOUR
+- Romain DICK
+- Jimmy GENSE
+- Nicolas MYLLE
---
+++ src/test/java/com/lacatho/miii/emargement/EmargementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lacatho.miii.emargement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmargementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
